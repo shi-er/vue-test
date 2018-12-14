7dd3d9ed3ed7df90969558f87cc0edce724cdb6f
shier <shier@ixiye.com>
2018-12-15 00:23:30 +0800

    添加项目

---
+++ README.md
@@ -0,0 +1,25 @@
+# admin
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+一个使用了layui的单页面模板和element ui的vue项目，主要目的是快速开发一些简单的网站
+
+使用方法都一样，
+cd到项目目录，然后
+npm install
+npm run dev
+
+
+最后附上项目的演示地址 https://dabeic.com/demo
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import login from '@/components/login'
+import index from '@/components/index'
+import welcome from '@/components/welcome'
+import customList from '@/components/customList'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: login
+    },
+    {
+      path: '/index',
+      component: index
+    },
+    {
+      path: '/welcome',
+      component: welcome
+    },
+    {
+      path: '/customList',
+      component: customList
+    }
+  ]
+})
