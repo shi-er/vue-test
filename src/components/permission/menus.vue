b313f641701259f032880772d4ff470f3780aed7
shier <shier@ixiye.com>
2018-12-16 00:08:44 +0800

    添加路由

---
+++ src/components/permission/menus.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "menus"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import login from '@/components/login'
 import index from '@/components/index'
 import welcome from '@/components/welcome'
 import customList from '@/components/customList'
+import permissionMenu from '../components/permission/menus';
+import permissionRole from '../components/permission/role';
+import permissionUser from '../components/permission/user';
 
 Vue.use(Router)
 
@@ -17,6 +20,23 @@ export default new Router({
     {
       path: '/index',
       component: index
+    }, {
+      path: '/permission',
+      component: permission,
+      children: [
+        {
+          path: '/permission/menu',
+          component: permissionMenu
+        },
+        {
+          path: '/permission/role',
+          component: permissionRole
+        },
+        {
+          path: '/permission/user',
+          component: permissionUser
+        },
+      ]
     },
     {
       path: '/welcome',
