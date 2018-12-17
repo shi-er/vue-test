ce274565c03b1a90219e6773fc8a49a9aa01d4ca
shier <shier@ixiye.com>
2018-12-18 00:06:41 +0800

    优化

--- src/components/userCenter/user/userInfoMenu.vue
+++ src/components/userCenter/user/userInfoMenu.vue
@@ -7,16 +7,7 @@
           <el-button type="primary" @click="getData" icon="el-icon-search">查询</el-button>
         </el-form-item>
         <el-form-item style="float: right">
-          <el-select v-model="type" placeholder="请选择类型">
-            <el-option label="菜单" value="0"></el-option>
-            <el-option label="功能" value="1"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item style="float: right">
-          <el-input placeholder="权限code" v-model="permCode"/>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" v-on:click="addNewProject({})" icon="el-icon-plus">添加</el-button>
+          <el-input placeholder="手机号" v-model="mobile"/>
         </el-form-item>
       </el-form>
     </el-col>
@@ -24,59 +15,58 @@
       :data="rows"
       style="width: 100%">
       <el-table-column
-        label="id">
+        label="用户ID">
         <template slot-scope="scope">
-          <span>{{scope.row.id}}</span>
+          <span>{{scope.row.uid}}</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="PID">
+        label="手机号">
         <template slot-scope="scope">
-          <span>{{scope.row.pid}}</span>
+          <span>{{scope.row.mobile}}</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="名称">
+        label="姓名">
         <template slot-scope="scope">
-          <span>{{ scope.row.title }}</span>
+          <span>{{ scope.row.name }}</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="类型">
+        label="身份证">
         <template slot-scope="scope">
-          <span v-if="scope.row.type===0">菜单</span>
-          <span v-else>功能</span>
+          <span>{{ scope.row.idNumber }}</span>
         </template>
       </el-table-column>
       <el-table-column
         label="状态">
         <template slot-scope="scope">
-          <span v-if="scope.row.state===1" style="color: mediumblue">正常</span>
+          <span v-if="scope.row.status===1" style="color: mediumblue">正常</span>
           <span v-else style="color: red">禁用</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="地址">
+        label="等级">
         <template slot-scope="scope">
-          <span>{{scope.row.url}}</span>
+          <span>{{scope.row.level}}</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="权限code">
+        label="注册地址">
         <template slot-scope="scope">
-          <span>{{scope.row.permcode}}</span>
+          <span>{{scope.row.address}}</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="排序">
+        label="注册时间">
         <template slot-scope="scope">
-          <span>{{scope.row.sort}}</span>
+          <span>{{getCurrentDateTime(scope.row.createTime)}}</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="描述">
+        label="修改时间">
         <template slot-scope="scope">
-          <span>{{scope.row.description}}</span>
+          <span>{{getCurrentDateTime(scope.row.modifyTime)}}</span>
         </template>
       </el-table-column>
       <el-table-column label="操作" min-width="171">
@@ -85,26 +75,6 @@
             size="mini"
             type="primary"
             @click="addNewProject(scope.row)"><i class="el-icon-edit"></i>编辑
-          </el-button>
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.row.id)"><i class="el-icon-delete"></i>删除
-          </el-button>
-          <el-button
-            size="mini"
-            type="success"
-            @click="handleSetSort(scope.row.id,scope.row.sort,0)"><i class="el-icon-caret-top"></i>置顶
-          </el-button>
-          <el-button
-            size="mini"
-            type="success"
-            @click="handleSetSort(scope.row.id,scope.row.sort,1)"><i class="el-icon-caret-top"></i>上移
-          </el-button>
-          <el-button
-            size="mini"
-            type="warning"
-            @click="handleSetSort(scope.row.id,scope.row.sort,2)"><i class="el-icon-caret-bottom"></i>下移
           </el-button>
         </template>
       </el-table-column>
@@ -127,8 +97,66 @@
 </template>
 
 <script>
+  import axios from 'axios';
+  import qs from 'qs';
+  import moment from "moment";
+
   export default {
     name: "userInfoMenu",
+    data() {
+      return {
+        dialogTableVisible: false,
+        mobile: '',
+        total: 0,
+        currentPage: 1,
+        pageSize: 10,
+        rows: [],
+        rowData: {}
+      }
+    },
+    methods: {
+      handleCurrentChange(val) {
+        this.currentPage = val;
+        this.getData();
+      },
+      handleSizeChange(val) {
+        this.pageSize = val;
+        this.getData();
+      },
+      //获取列表
+      getData() {
+        let url = '/user/base/getList?_index=' + ((this.currentPage - 1) * this.pageSize) + '&_size=' + this.pageSize;
+        if (this.mobile !== '') {
+          url = url + "&mobile=" + this.mobile;
+        }
+        axios.get(url,
+          qs.stringify({})).then((response) => {
+          this.rows = response.data.rows;
+          this.total = response.data.total;
+        }).catch((error) => {
+          console.log(error);
+        });
+      },
+
+      getCurrentDateTime(date) {
+        return moment(date).format('YYYY-MM-DD HH:mm:ss')
+      },
+      dialogClose() {
+        this.$emit('leftMenuStatus', true);
+      },
+      successClose(data) {
+        this.dialogTableVisible = false;
+        if (data != "cancel") {
+          this.editData(data);
+        }
+      },
+      getCurrentDateTime(date) {
+        return moment(date).format('YYYY-MM-DD HH:mm:ss')
+      },
+    },
+    mounted() {
+      this.getData();
+    },
 
   }
 </script>
--- src/components/userCenter/userLogin/loginMenu.vue
+++ src/components/userCenter/userLogin/loginMenu.vue
@@ -7,16 +7,7 @@
           <el-button type="primary" @click="getData" icon="el-icon-search">查询</el-button>
         </el-form-item>
         <el-form-item style="float: right">
-          <el-select v-model="type" placeholder="请选择类型">
-            <el-option label="菜单" value="0"></el-option>
-            <el-option label="功能" value="1"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item style="float: right">
-          <el-input placeholder="权限code" v-model="permCode"/>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" v-on:click="addNewProject({})" icon="el-icon-plus">添加</el-button>
+          <el-input placeholder="手机号" v-model="mobile"/>
         </el-form-item>
       </el-form>
     </el-col>
@@ -24,59 +15,33 @@
       :data="rows"
       style="width: 100%">
       <el-table-column
-        label="id">
+        label="ID">
         <template slot-scope="scope">
           <span>{{scope.row.id}}</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="PID">
+        label="用户ID">
         <template slot-scope="scope">
-          <span>{{scope.row.pid}}</span>
+          <span>{{scope.row.uid}}</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="名称">
+        label="用户名">
         <template slot-scope="scope">
-          <span>{{ scope.row.title }}</span>
+          <span>{{ scope.row.realname }}</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="类型">
+        label="登录地址">
         <template slot-scope="scope">
-          <span v-if="scope.row.type===0">菜单</span>
-          <span v-else>功能</span>
+          <span>{{ JSON.parse(scope.row.loginAdress).address }}</span>
         </template>
       </el-table-column>
       <el-table-column
-        label="状态">
+        label="登录时间">
         <template slot-scope="scope">
-          <span v-if="scope.row.state===1" style="color: mediumblue">正常</span>
-          <span v-else style="color: red">禁用</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="地址">
-        <template slot-scope="scope">
-          <span>{{scope.row.url}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="权限code">
-        <template slot-scope="scope">
-          <span>{{scope.row.permcode}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="排序">
-        <template slot-scope="scope">
-          <span>{{scope.row.sort}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="描述">
-        <template slot-scope="scope">
-          <span>{{scope.row.description}}</span>
+          <span>{{getCurrentDateTime(scope.row.loginTime)}}</span>
         </template>
       </el-table-column>
       <el-table-column label="操作" min-width="171">
@@ -85,26 +50,6 @@
             size="mini"
             type="primary"
             @click="addNewProject(scope.row)"><i class="el-icon-edit"></i>编辑
-          </el-button>
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.row.id)"><i class="el-icon-delete"></i>删除
-          </el-button>
-          <el-button
-            size="mini"
-            type="success"
-            @click="handleSetSort(scope.row.id,scope.row.sort,0)"><i class="el-icon-caret-top"></i>置顶
-          </el-button>
-          <el-button
-            size="mini"
-            type="success"
-            @click="handleSetSort(scope.row.id,scope.row.sort,1)"><i class="el-icon-caret-top"></i>上移
-          </el-button>
-          <el-button
-            size="mini"
-            type="warning"
-            @click="handleSetSort(scope.row.id,scope.row.sort,2)"><i class="el-icon-caret-bottom"></i>下移
           </el-button>
         </template>
       </el-table-column>
@@ -127,9 +72,67 @@
 </template>
 
 <script>
-    export default {
-        name: "loginMenu"
-    }
+  import axios from 'axios';
+  import qs from 'qs';
+  import moment from "moment";
+
+  export default {
+    name: "loginMenu",
+    data() {
+      return {
+        dialogTableVisible: false,
+        mobile: '',
+        total: 0,
+        currentPage: 1,
+        pageSize: 10,
+        rows: [],
+        rowData: {}
+      }
+    },
+    methods: {
+      handleCurrentChange(val) {
+        this.currentPage = val;
+        this.getData();
+      },
+      handleSizeChange(val) {
+        this.pageSize = val;
+        this.getData();
+      },
+      //获取列表
+      getData() {
+        let url = '/user/login/getList?_index=' + ((this.currentPage - 1) * this.pageSize) + '&_size=' + this.pageSize;
+        if (this.mobile !== '') {
+          url = url + "&mobile=" + this.mobile;
+        }
+        axios.get(url,
+          qs.stringify({})).then((response) => {
+          this.rows = response.data.rows;
+          this.total = response.data.total;
+        }).catch((error) => {
+          console.log(error);
+        });
+      },
+
+      getCurrentDateTime(date) {
+        return moment(date).format('YYYY-MM-DD HH:mm:ss')
+      },
+      dialogClose() {
+        this.$emit('leftMenuStatus', true);
+      },
+      successClose(data) {
+        this.dialogTableVisible = false;
+        if (data != "cancel") {
+          this.editData(data);
+        }
+      },
+      getCurrentDateTime(date) {
+        return moment(date).format('YYYY-MM-DD HH:mm:ss')
+      },
+    },
+    mounted() {
+      this.getData();
+    },
+  }
 </script>
 
 <style scoped>
